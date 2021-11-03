e6c226bfa2ebe6b7972dd4e33d9b9d10397a1af4
Rossellinie Sanchez <rosse.nie57@gmail.com>
2021-11-02 21:34:28 -0400

    components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Header from './Header';
+import Nav from './Nav';
+import Stories from "./Stories";
+
+
+const navItems = ["arts", "books", "fashion", "food", "movies", "travel"];
+const nytapi = "k3EOCYL8tL0QZfGchGecvzgvC2Y9Mqni";
+const section = "arts";
 
 function App() {
+  const [stories, setStories] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${nytapi}`
+    )
+      .then((response) => response.json())
+      .then((data) => setStories(data.results));
+  }, []);
+
   return (
-    <div>
-      <h4>Let's go!</h4>
-    </div>
+    < >
+      <Header siteTitle="All the news the Fits We Print" />
+      <Nav navItems={navItems} />
+      <Stories stories={stories}/>
+    </>
   );
 }
 
